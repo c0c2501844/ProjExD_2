442333f545805cf3514c080f9465b4b936c4891f
katakura <c0c2501844@edu.teu.ac.jp>
2026-06-16 14:49:55 +0900

    練習3: こうかとんと爆弾が画面外に出ないようにした

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,23 +5,39 @@ import random
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, +5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (+5, 0)}
-vx = +5
-vy = +5
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    # 引数：こうかとんRect or 爆弾Rect
+    # 戻り値：横方向、縦方向の真理値タプル（画面内：True／画面外：False）
+    # Rectオブジェクトのleft, right, top, bottomの値から画面内・外を判断する
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
+
+    # こうかとんの初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    # 爆弾の初期化
+    vx, vy = +5, +5
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -32,13 +48,25 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
         for k, mv in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        bb_rct.move_ip(vx, vy)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        # 更新後の座標が画面外になった場合の挙動
+        # kk_rct：更新前の位置に戻す
+        # bb_rct：横（縦）方向に出そうになったらvx（vy）の符号を反転する
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
